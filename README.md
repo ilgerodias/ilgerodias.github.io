10ba40b433cb18311887b40fd67ca5e7e0d23d42
ilgerodias <igerodias@hotmail.com>
2018-08-15 16:02:18 -0700

    removed text boxes on home page

---
+++ README.md
@@ -0,0 +1,4 @@
+# ilgerodias.github.io
+Website
+
+Here is my portfolio website, still a WIP, but soon to be officially deployed
---
+++ index.js
@@ -0,0 +1,68 @@
+class FirstSection extends React.Component {
+  // necessary code for typed.js to work with react (credit to mattboldt)
+  componentDidMount() {
+   const { strings } = this.props;
+   const options = {
+     strings: strings,
+     typeSpeed: 30,
+     backSpeed: 30
+   };
+   this.typed = new Typed(this.el, options);
+ }
+ componentWillUnmount() {
+   this.typed.destroy();
+ }
+  render() {
+    return <div><a href="#">
+    <div className="container-fluid siteStart">
+        <h1><span
+            style={{ whiteSpace: 'pre' }}
+            ref={(el) => { this.el = el; }}
+          /></h1>
+    </div>
+    </a>
+</div>
+  }
+}
+
+class SecondSection extends React.Component {
+  render() {
+    return <div><a href="#">
+    <div className="container-fluid secondPart">
+      <h1 className="display-4">About Me</h1>
+    </div>
+    </a>
+</div>
+  }
+}
+
+class ThirdSection extends React.Component {
+  render() {
+    return <div>
+  <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+    <h1 className="display-4">My Projects</h1>
+    </div>
+  </div>
+</div>
+  }
+}
+
+class MyWebsite extends React.Component {
+  render() {
+    return <div>
+      <FirstSection
+      strings={[
+    	'Hi there!',
+      'Welcome to my portfolio,',
+      'My name is Ian Gerodias.',
+      'Have a look around!']}/>
+      <SecondSection/>
+      <ThirdSection/>
+      </div>
+  }
+}
+ReactDOM.render(
+<MyWebsite/>,
+document.getElementById('root')
+)
